aa7ace1d6373e10b3cfcfda5e3a66d7c413a67b1
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2017-03-10 17:27:50 +0100

    remove process argument

--- MG5_aMCatNLO/htbinning.py
+++ MG5_aMCatNLO/htbinning.py
@@ -1,30 +1,30 @@
 import json
 import os
 
-def writecards(binning, incard):
-    outdir=incard.replace("run_card.dat","")
+def writecards(binning, indir):
+    
+    incard = indir + '/run_card.dat'
+    
     with open(incard) as f:
         infile = f.readlines()
-    for i in xrange(0,len(binning)-1):
-        if os.path.isdir("%sHTbinning%i_%i"%(outdir,binning[i],binning[i+1]))==False:
-            cmd="mkdir %sHTbinning%i_%i"%(outdir,binning[i],binning[i+1])
+        for i in xrange(0,len(binning)-1):
+          if os.path.isdir("%sHT_%i_%i"%(indir,binning[i],binning[i+1]))==False:
+            cmd="mkdir %sHT_%i_%i"%(indir,binning[i],binning[i+1])
             os.system(cmd)
-            cmd='cp %sproc_card.dat %sHTbinning%i_%i'%(outdir,outdir,binning[i],binning[i+1])
+            cmd='cp %sproc_card.dat %sHT_%i_%i'%(indir,indir,binning[i],binning[i+1])
             os.system(cmd)
-            if os.path.isfile('%scuts.f'%(outdir)):
-                cmd='cp %scuts.f %sHTbinning%i_%i'%(outdir,outdir,binning[i],binning[i+1])
-                os.system(cmd)
+          if os.path.isfile('%scuts.f'%(indir)):
+            cmd='cp %scuts.f %sHT_%i_%i'%(indir,indir,binning[i],binning[i+1])
+            os.system(cmd)
 
             
-        for line in xrange(len(infile)):
+          for line in xrange(len(infile)):
             if 'ihtmin' in infile[line] :
-                print infile[line]
                 infile[line]='%.1f = ihtmin  !inclusive Ht for all partons (including b)\n'%(binning[i])
             if 'ihtmax' in infile[line]:
-                print infile[line]
                 infile[line]='%.1f = ihtmax  !inclusive Ht for all partons (including b)\n'%(binning[i+1])
-        with open("%sHTbinning%i_%i/run_card.dat"%(outdir,binning[i],binning[i+1]), "w") as f1:
-            f1.writelines(infile)            
+          with open("%sHT_%i_%i/run_card.dat"%(indir,binning[i],binning[i+1]), "w") as f1:
+            f1.writelines(infile)        
 #__________________________________________________________
 if __name__=="__main__":
     Dir = os.getcwd()
@@ -32,12 +32,8 @@ if __name__=="__main__":
     from optparse import OptionParser
     parser = OptionParser()
 
-    parser.add_option ('-i','--inputcard', help='input madgraph run card',
-                       dest='inputcard',
-                       default='')
-
-    parser.add_option ('-p', '--process',  help='process to bin',
-                       dest='process',
+    parser.add_option ('-i','--inputdir', help='input process dir',
+                       dest='inputdir',
                        default='')
 
     parser.add_option ('-b', '--binning',  help='binning file',
@@ -45,12 +41,13 @@ if __name__=="__main__":
                        default='')
 
     (options, args) = parser.parse_args()
-    inputcard  = options.inputcard
-    process    = options.process
-    binning    = options.binning
-    
+    inputdir  = options.inputdir
+    binning   = options.binning
+     
+    process = os.path.split(os.path.abspath(inputdir))[1]
+
     with open(binning) as binning_file:    
         data = json.load(binning_file)
     print data[process]
     
-    writecards(data[process], inputcard)
+    writecards(data[process], inputdir)
